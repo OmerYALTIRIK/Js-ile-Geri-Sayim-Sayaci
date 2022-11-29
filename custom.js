3013078711180ace3d2c216f942a097dbb39ef88
Ömer Yaltırık <40443652+OmerYALTIRIK@users.noreply.github.com>
2022-11-29 23:37:32 +0300

    first commit

---
+++ custom.js
@@ -0,0 +1,31 @@
+var deger = document.getElementById("degeri_al");
+var programla = document.getElementById("programla");
+var baslat = document.getElementById("baslat");
+var durdur = document.getElementById("durdur");
+var kutu = document.getElementsByClassName("kutu")[0];
+var durum = document.getElementsByClassName("durum")[0];
+var saniye = document.getElementsByClassName("deger")[0];
+
+var x;
+programla.addEventListener("click", () => {
+  saniye.innerHTML = deger.value;
+  durum.classList.remove("gorun");
+});
+baslat.addEventListener("click", () => {
+  x = setInterval(geriSay, 1000);
+  function geriSay() {
+    var degeri_al = Number(saniye.textContent);
+    if (degeri_al > 0) {
+      degeri_al -= 1;
+      saniye.innerHTML = degeri_al.toString();
+      kutu.classList.toggle("tetik");
+    } else {
+      clearInterval(x);
+      durum.classList.add("gorun");
+    }
+  }
+});
+
+durdur.addEventListener("click", () => {
+  clearInterval(x);
+});
